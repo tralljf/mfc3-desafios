882f4bcf60a69ec56f93e99e4d9fa114a296ee91
Thiago Rabelo <thiagorabelojf@gmail.com>
2020-06-30 20:56:16 -0300

    Arquivos do desafio da Maratona Full Cycle 3

---
+++ desafio-02/actions/home.go
@@ -0,0 +1,18 @@
+package actions
+
+import (
+	"net/http"
+
+	"github.com/gobuffalo/buffalo"
+)
+
+// HomeHandler is a default handler to serve up
+// a home page.
+func HomeHandler(c buffalo.Context) error {
+	return c.Render(http.StatusOK, r.JSON(map[string]string{"message": "Welcome to Buffalo!"}))
+}
+
+
+func Hello(c buffalo.Context) error {
+	return c.Render(http.StatusOK, r.JSON(map[string]string{"message": "Hello Full Cycle"}))
+}
